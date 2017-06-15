fb25376df02d3ef2636c9cf83bfec702800453f0
Denis Zvezdov <s1iderorama@gmail.com>
2017-06-15 22:02:26 +0300

    with homework

--- test_registration.py
+++ test_registration.py
@@ -1,6 +1,7 @@
 import unittest
 from selenium import webdriver
 from reg_page import RegistrationPage
+from random import randint
 
 
 class TestRegistration(unittest.TestCase):
@@ -29,3 +30,19 @@ class TestRegistration(unittest.TestCase):
         page.find_element('register_btn').click()
 
         self.assertTrue(page.is_password_validation_error)
+
+    def test_registration_success(self):
+        page = RegistrationPage(self.browser)
+
+        reg_data = {
+            'first_name': 'Vova',
+            'last_name': 'ZiLvova',
+            'email': 'vovan+{}@gmail.com'.format(randint(0, 100)),
+            'password': 'pass1',
+            'confirmation': 'pass2'
+        }
+
+        for field, value in reg_data.items():
+            page.find_element(field).send_keys(value)
+        page.find_element('newsletter_chbx').click()
+        page.find_element('register_btn').click()
