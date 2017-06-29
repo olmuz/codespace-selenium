41d98623cc45c1fc67554014b796de7800f6c7c4
Denis Zvezdov <s1iderorama@gmail.com>
2017-06-29 20:41:10 +0300

    comment

--- test_advanced_selenium_elements.py
+++ test_advanced_selenium_elements.py
@@ -75,7 +75,7 @@ class TestAlert(unittest.TestCase):
         ac.send_keys('T')
         ac.perform()
 
-        # switch to latest tab
+        # switch to latest tab 1
         window_name = self.browser.window_handles[-1]
         self.browser.switch_to.window(window_name)
         pages.RegistrationPage(self.browser).open()
